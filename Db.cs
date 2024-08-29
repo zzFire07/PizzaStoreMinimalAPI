0701bc404935cb923a287b37438f7bed43f4ed23
Giorgio <Giorgio.san007@gmail.com>
2024-08-29 20:19:08 -0300

    new project inited

---
+++ Db.cs
@@ -0,0 +1,95 @@
+﻿namespace PizzaStore.Db;
+
+public record Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+
+public class PizzaDB
+{
+
+    public static int Delay { get; set; }
+    public static bool Error { get; set; }
+
+    private static List<Pizza> _pizzas = new List<Pizza>()
+   {
+     new Pizza{ Id=1, Name="Montemagno, Pizza shaped like a great mountain" },
+     new Pizza{ Id=2, Name="The Galloway, Pizza shaped like a submarine, silent but deadly"},
+     new Pizza{ Id=3, Name="The Noring, Pizza shaped like a Viking helmet, where's the mead"}
+   };
+
+    public static List<Pizza> GetPizzas()
+    {
+        if (Delay > 0)
+        {
+            Thread.Sleep(Delay);
+        }
+        if (Error)
+        {
+            throw new Exception("");
+        }
+        return _pizzas;
+    }
+
+    public static Pizza? GetPizza(int id)
+    {
+        if (Delay > 0)
+        {
+            Thread.Sleep(Delay);
+        }
+        if (Error)
+        {
+            throw new Exception("");
+        }
+        return _pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+
+    public static Pizza CreatePizza(Pizza pizza)
+    {
+        if (Delay > 0)
+        {
+            Thread.Sleep(Delay);
+        }
+        if (Error)
+        {
+            throw new Exception("");
+        }
+        _pizzas.Add(pizza);
+        return pizza;
+    }
+
+    public static Pizza UpdatePizza(Pizza update)
+    {
+        if (Delay > 0)
+        {
+            Thread.Sleep(Delay);
+        }
+        if (Error)
+        {
+            throw new Exception("");
+        }
+        _pizzas = _pizzas.Select(pizza =>
+        {
+            if (pizza.Id == update.Id)
+            {
+                pizza.Name = update.Name;
+            }
+            return pizza;
+        }).ToList();
+        return update;
+    }
+
+    public static void RemovePizza(int id)
+    {
+        if (Delay > 0)
+        {
+            Thread.Sleep(Delay);
+        }
+        if (Error)
+        {
+            throw new Exception("");
+        }
+        _pizzas = _pizzas.FindAll(pizza => pizza.Id != id).ToList();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.OpenApi.Models;
+using PizzaStore.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen(c =>
+      {
+          c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pizza Santi API", Description = "Keep a documentation for our API's", Version = "v1" });
+      });
+}
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizza Santi API V1");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
+app.MapPost("/delay/{ms}", (int ms) => PizzaDB.Delay = ms);
+app.MapGet("/delay/", () => PizzaDB.Delay);
+
+app.MapPost("/error/{onOff}", (bool onOff) => PizzaDB.Error = onOff);
+app.MapGet("/error/", () => PizzaDB.Error);
+
+app.Run();
